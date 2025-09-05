90ed3bd7fb5165a5b2b10ce2f2d2c1070fc6e744
TracyMcBean <thkisz@yahoo.co.uk>
2025-09-05 09:46:18 +0300

    Restructure code to avoid saving temporary netcdf files

--- example_tropical_cyclone/main.py
+++ example_tropical_cyclone/main.py
@@ -16,7 +16,7 @@ from pathlib import Path
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 import huracanpy
-from tracker import detectnodes, stitchnodes
+from tracker import detectnodes, stitchnodes, combine_all_nodes
 from preprocessing import preprocessing
 
 #### Modify ------------------------------------------------------------------
@@ -46,9 +46,10 @@ plot_path= f"{CURRENT_DIR}/plots/"  # Define plot output location
 
 # Define start and end dates
 start_date = datetime.strptime("2035-06-01", "%Y-%m-%d")
-end_date = datetime.strptime("2035-06-05", "%Y-%m-%d") #"2035-07-31"
-
-out_file = f"{track_path}/icon_{start_date}_{end_date}.csv" #File where the tracks will be stored
+end_date = datetime.strptime("2035-06-10", "%Y-%m-%d") #"2035-07-31"
+formatted_sdate= start_date.strftime("%Y%m%d")
+formatted_edate = end_date.strftime("%Y%m%d")
+track_file = f"{track_path}/icon_{start_date.strftime("%Y%m%d")}_{end_date.strftime("%Y%m%d")}.csv" #File where the tracks will be stored
 
 #### End mondify ---------------------------------------------------------------------------------
 
@@ -64,16 +65,36 @@ datelist = [
     for i in range((end_date - start_date).days + 1)
 ]
 
-#preprocessing the data
-preprocessing(datelist, ncfile_prefix, request_basic)
+for date in datelist:
+    nc_file = f"{ncfile_prefix}_{date}.nc"
+    nodes_subpath=f"{nodes_path}/{date[:4]}/{date[4:6]}"
+    nodes_file = f"{nodes_subpath}/nodes_{date}.txt"
+    Path(nodes_subpath).mkdir(parents=True, exist_ok=True)
+    
+    # Skip date if output already exists
+    if Path(nodes_file).exists():
+        print(f"{nodes_file} exists, skipping...")
+        continue
+    
+    print(f"Processing {date}")
+    
+    #preprocessing the data
+    preprocessing(date, nc_file, request_basic)
+    # run nodes detection
+    detectnodes(nodes_file, nc_file)
+    
+    # remove netcdf files so they don't use too much space
+    print(f"Removing {nc_file}")
+    os.remove(nc_file) 
+   # return
 
-# run nodes detection
-all_nodes_file = detectnodes(datelist, nodes_path, ncfile_prefix)
+# combine all node files into one file
+all_nodes_file = combine_all_nodes(nodes_path, formatted_sdate, formatted_edate)
 # run nodes stitching
-stitchnodes(all_nodes_file, out_file)
+stitchnodes(all_nodes_file, track_file)
 
 # Create a plot and save it
-tracks = huracanpy.load(out_file)
+tracks = huracanpy.load(track_file)
 huracanpy.plot.tracks(tracks.lon, tracks.lat, intensity_var=tracks.wind)
-plt.title(f"ICON SSP3-7.0 {start_date} to {end_date}")
-plt.savefig(f"{plot_path}/icon_projection_tc_tracks_wind_{start_date}_{end_date}.png")
+plt.title(f"ICON SSP3-7.0 {formatted_sdate} to {formatted_edate}")
+plt.savefig(f"{plot_path}/icon_projection_tc_tracks_wind_{formatted_sdate}_{formatted_edate}.png")
--- example_tropical_cyclone/tracker.py
+++ example_tropical_cyclone/tracker.py
@@ -11,39 +11,31 @@ import subprocess
 import glob
 from pathlib import Path
 
-def detectnodes(datelist:list,nodes_path:str,ncfile_prefix:str)->str:
+def detectnodes(nodes_file:str,nc_file:str)->str:
     # For every day get the data from the data-bridge and stores it as netcdf file. 
     # Then run the DetectNodes processing to detect the tropical cyclones
-    for date in datelist:
-    
-        in_file = f"{ncfile_prefix}_{date}.nc"
-        out_file = f"{nodes_path}/nodes_{date}.txt"
-    
-        # Skip date if output already exists
-        if Path(out_file).exists():
-            print(f"{out_file} exists, skipping...")
-            continue
-    
-        print(f"Processing {date}")
                 
-        # Here the TempestExtremes function DetectNodes is called.
-        subprocess.run([
-            "DetectNodes",
-            "--in_data", in_file,
-            "--out", out_file,
-            "--searchbymin", "msl",
-            "--latname", "latitude",
-            "--lonname", "longitude",
-            "--closedcontourcmd", "msl,200.0,5.5,0;_DIFF(zg250,zg500),-6,6.5,1.0",
-            "--mergedist", "6.0",
-            "--outputcmd", "msl,min,0;_VECMAG(uas,vas),max,2"
-        ])
-        
-        subprocess.run(["head", out_file])
+    # Here the TempestExtremes function DetectNodes is called.
+    subprocess.run([
+        "DetectNodes",
+        "--in_data", nc_file,
+        "--out", nodes_file,
+        "--searchbymin", "msl",
+        "--latname", "latitude",
+        "--lonname", "longitude",
+        "--closedcontourcmd", "msl,200.0,5.5,0;_DIFF(zg250,zg500),-6,6.5,1.0",
+        "--mergedist", "6.0",
+        "--outputcmd", "msl,min,0;_VECMAG(uas,vas),max,2"
+    ])
     
+    #if verbose: 
+    #    subprocess.run(["head", nodes_file])
+    return
+
+def combine_all_nodes(nodes_path:str,start:str, end:str)->str:
     # now combine all files 
-    flist = glob.glob(f'{nodes_path}/*.txt')
-    all_nodes = f'{nodes_path}/all_nodes.dat'
+    flist = glob.glob(f'{nodes_path}/*/*/*.txt')
+    all_nodes = f'{nodes_path}/all_nodes_{start}_{end}.dat'
     
     with open(all_nodes, 'w') as outfile:
         for fname in flist:
