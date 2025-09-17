651da9c829ca59f2cba3fb18c4b16161faebd03d
TracyMcBean <thkisz@yahoo.co.uk>
2025-09-17 10:37:49 +0300

    Improved useability

--- example_tools/parallel_data_download.py
+++ example_tools/parallel_data_download.py
@@ -5,22 +5,27 @@
 #
 # Request file should be a json file. Name doesn't matter if specified as argument. If not it will look for a file called request.json.
 #
-# Run as: python parallel_data_download.py <number_of_threads> <start_date_YYYY-MM-DD> <end_date_YYYY-MM-DD> <request.json>
+# If run without arguments, default values will be used. 
+# With arguments:
+# python parallel_data_download.py --num_threads 8 --start_date 2023-06-01 --end_date 2023-06-10 --request_file custom_request.json --output_prefix ./output/data
+
 
 from get_climate_dt_data import get_data_as_file
 
 from concurrent.futures import ThreadPoolExecutor, as_completed
-import sys
 from datetime import datetime, timedelta
 import json
+import argparse
 
-def download_date(date, request, output_path):
+def download_date(date:str, request:dict, output_path:str):
     """
     This function downloads data for a specific date.
 
     Parameters:
     ----------
-    date: string
+    date: Requested day
+    request: The data request dictionary
+    output_path: Location where data will be stored
     """
     print(f'Requesting date {date}')
     output_file = output_path+'_'+date+'.grib'
@@ -32,21 +37,39 @@ def main():
     """
     Using multiple threads, download the data.
     """
-    n = int(sys.argv[1]) # number of threads
-    start_date = datetime.strptime(sys.argv[2], "%Y-%m-%d")
-    end_date = datetime.strptime(sys.argv[3], "%Y-%m-%d")
+    parser = argparse.ArgumentParser(description="Download data using multiple threads.")
+    parser.add_argument("-n", "--num_threads", type=int, default=4,
+                        help="Number of threads to use (default: 4)")
+    parser.add_argument("-s", "--start_date", type=str, default="2023-01-01",
+                        help="Start date in YYYY-MM-DD format (default: 2023-01-01)")
+    parser.add_argument("-e", "--end_date", type=str, default="2023-01-10",
+                        help="End date in YYYY-MM-DD format (default: 2023-01-10)")
+    parser.add_argument("-r", "--request_file", type=str, default="request.json",
+                        help="Path to request JSON file (default: request.json)")
+    parser.add_argument("-o", "--output_prefix", type=str, default="./data/test",
+                        help="Output prefix path (default: ./data/test)")
 
-    # create request from request file
-    if len(sys.argv) > 4:
-        with open(sys.argv[4], 'r') as f:
+    args = parser.parse_args()
+
+    # Validate date format
+    try:
+        start_date = datetime.strptime(args.start_date, "%Y-%m-%d")
+        end_date = datetime.strptime(args.end_date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Dates must be in YYYY-MM-DD format.")
+
+    if start_date > end_date:
+        raise ValueError("Start date must be before or equal to end date.")
+
+    # Load request file
+    try:
+        with open(args.request_file, 'r') as f:
             request = json.load(f)
-    else:
-        with open("request.json", 'r') as f:
-            request = json.load(f)
-    
-    # Set the output path
-    # TODO: Make this variable!
-    output_path="./data/test"
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Request file '{args.request_file}' not found.")
+    except json.JSONDecodeError:
+        raise ValueError(f"Request file '{args.request_file}' is not valid JSON.")
+
 
     # Create list of dates
     datelist = [
@@ -54,10 +77,12 @@ def main():
     for i in range((end_date - start_date).days + 1)
     ]
     
-    with ThreadPoolExecutor(max_workers=n) as executor:
-        futures = [executor.submit(download_date, date, request, output_path) for date in datelist]
+    # run download in parallel.
+    with ThreadPoolExecutor(max_workers=args.num_threads) as executor:
+        futures = [executor.submit(download_date, date, request, args.output_prefix) for date in datelist]
         for future in as_completed(futures):
             future.result()  # This will raise exceptions if any occurred
     return
 
-main()
+if __name__ == "__main__":
+    main()
